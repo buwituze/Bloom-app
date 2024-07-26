6fa2ef6e853f791717a3e3cd3e3658b0ed325ed1
buwituze <b.rutagengw@alustudent.com>
2024-07-27 00:25:28 +0200

    commit

---
+++ Bloom/src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/login";
+import Navbar from "./components/Navbar";
+import Signup from "./components/signup";
+import AllProducts from "./components/allproducts";
+import BookingProducts from "./components/bookingpdts";
+import Fertilizers from "./components/fertilizers";
+import Seeds from "./components/seeds";
+import Pesticides from "./components/pesticides";
+import FarmingTools from "./components/farmingtools";
+import IrrigationTools from "./components/irrigationtools";
+import TestKits from "./components/testkits";
+import FarmProduce from "./components/farmproduce";
+import Resources from "./components/resources";
+import News from "./components/news";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/allproducts" element={<AllProducts />} />
+        <Route path="/bookingpdts" element={<BookingProducts />} />
+        <Route path="/fertilizers" element={<Fertilizers />} />
+        <Route path="/seeds" element={<Seeds />} />
+        <Route path="/pesticides" element={<Pesticides />} />
+        <Route path="/farmingtools" element={<FarmingTools />} />
+        <Route path="/irrigationtools" element={<IrrigationTools />} />
+        <Route path="/testkits" element={<TestKits />} />
+        <Route path="/farmproduce" element={<FarmProduce />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
